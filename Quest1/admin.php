2b7308cd1e824b48df841d4e8ff1723657199ece
uchejudeegbue <lordadmiralty@yahoo.com>
2017-03-02 13:33:46 +0100

    ss

--- Quest1/admin.php
+++ Quest1/admin.php
@@ -13,18 +13,11 @@ $query = "SELECT * FROM responce";
 $result = mysql_query($query);
 while($row = mysql_fetch_array($result, MYSQL_ASSOC))
 {
-echo "User ID :{$row['user_ID']} <br>" .
-"Age : {$row['response1']} <br>" . 
-"clanwebsite : {$row['clanwebsite']} <br><br>";
 
-  echo '<table border="5" cellpadding="5" cellspacing="0" style="border-collapse: collapse" bordercolor="#808080" width="100%" id="AutoNumber2" bgcolor="#C0C0C0">
-  <tr>
-    <td width="14%" align="center">User ID</td>
-    <td width="14%" align="center">Details</td>
+echo "<th>User ID :</th>
+    <th>Age :</th>". "<td>{$row['user_ID']}</td>" .
+"<td>{$row['response1']} </td>";
 
-</tr>
-</table>';
-}
 mysql_close($conn);
 
 ?>
