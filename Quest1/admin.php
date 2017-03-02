9416c8b693a068ecc73837d4737a176de54ba790
uchejudeegbue <lordadmiralty@yahoo.com>
2017-03-02 13:28:30 +0100

    ss

--- Quest1/admin.php
+++ Quest1/admin.php
@@ -16,6 +16,14 @@ while($row = mysql_fetch_array($result, MYSQL_ASSOC))
 echo "User ID :{$row['user_ID']} <br>" .
 "Age : {$row['response1']} <br>" . 
 "clanwebsite : {$row['clanwebsite']} <br><br>";
+
+  echo '<table border="5" cellpadding="5" cellspacing="0" style="border-collapse: collapse" bordercolor="#808080" width="100%" id="AutoNumber2" bgcolor="#C0C0C0">
+  <tr>
+    <td width="14%" align="center">User ID</td>
+    <td width="14%" align="center">Details</td>
+
+</tr>
+</table>';
 }
 mysql_close($conn);
 
